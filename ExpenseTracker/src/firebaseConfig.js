53ca6de280de73352d7c2459284fc801fb3ed60a
chrisRV15 <christopher.d.thomasvillalobos@lions.enc.edu>
2024-10-30 22:17:24 -0600

    Fix security issue

--- ExpenseTracker/src/firebaseConfig.js
+++ ExpenseTracker/src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyADpKILRtkEsk0DCaCppqSK_BzTALupV6Q",
-    authDomain: "expensetracker-4255c.firebaseapp.com",
-    projectId: "expensetracker-4255c",
-    storageBucket: "expensetracker-4255c.appspot.com",
-    messagingSenderId: "914204917667",
-    appId: "1:914204917667:web:c0fe422982c483d4aea416",
-    measurementId: "G-7YYRFLKE3S"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
